434a7f58b723007a9a7019ac7c6e4eb9d5721897
he.ziyao <he.ziyao@uni-konstanz.de>
2024-02-05 16:53:30 +0100

    update

--- LineFollowing.py
+++ LineFollowing.py
@@ -94,14 +94,14 @@ last_dir = None
 last_angle = None
 # pid controller over the angle
 #controller = PID(1, 0.1, 0.05, setpoint=0, output_limits=(-3.14, 3.14), starting_output=3.14, sample_time=1. / 30.)
-controller = PID(1, 0.1, 0.05, setpoint=320) # 320 is the mid point of the image in x direction
+controller = PID(1, 0.1, 0.05, setpoint=320, starting_output=3.14) # 320 is the mid point of the image in x direction
 for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=True):
     image = frame.array
     img_bottom = image[-100:, :]
 
     img, dir, angle,centroid_x = LineDetection.preprocessImage(img_bottom)
     if (dir != None):
-        angular_v = controller(angle)
+        angular_v = controller(angle) -3.14
         # linear_v = 400 - abs(angular_v * 100 / 3.14)
         linear_v = 300
         angular_v = angular_v * 30  # remap to (-100, 100), left positive, right negative
