064c6b2e8f10e379e5fa90520379a05a86a453f8
he.ziyao <he.ziyao@uni-konstanz.de>
2024-02-19 16:02:28 +0100

    update

--- line_follow_cam.py
+++ line_follow_cam.py
@@ -188,7 +188,7 @@ def control_car(dry_run=False):
 
         angular_v = controller(current_position) - 3.14
         # linear_v = 400 - abs(angular_v * 100 / 3.14)
-        linear_v = 200
+        linear_v = 300
         angular_v = angular_v * 5  # remap to (-100, 100), left positive, right negative
 
         if (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
