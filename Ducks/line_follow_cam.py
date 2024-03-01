8431b37fa73b48e4d9537d07ee942f4a0ab7a953
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-01 16:54:53 +0100

    update

--- Ducks/line_follow_cam.py
+++ Ducks/line_follow_cam.py
@@ -188,8 +188,8 @@ def control_car(dry_run=False):
 
         angular_v = controller(current_position) - 3.14
         # linear_v = 400 - abs(angular_v * 100 / 3.14)
-        angular_v*=30
-        linear_v = 300
+        angular_v *=30
+        linear_v = 450
         if (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
             linear_v = 0
             angular_v = angular_v * 5
