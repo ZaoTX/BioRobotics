188cc1a4df1a3110fb3ef7c00175bf7c82467afc
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-01 16:28:02 +0100

    update

--- line_follow_cam.py
+++ line_follow_cam.py
@@ -189,9 +189,9 @@ def control_car():
 
         # linear_v = 400 - abs(angular_v * 100 / 3.14)
         linear_v = 300
-        if (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
-            linear_v = 0
-            angular_v = angular_v * 3
+        # if (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
+        #     linear_v = 0
+        #     angular_v = angular_v * 3
 
         set_car_control(linear_v, angular_v)
         print(f"Set speed lin: {linear_v}, ang: {angular_v}")
