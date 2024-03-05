1ad0d6e0e6311c3a993fad1a132bd005143b6318
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-05 15:36:28 +0100

    update

--- Ziyao/Mindduck.py
+++ Ziyao/Mindduck.py
@@ -125,7 +125,7 @@ def avoid_duck(linv_ori, angv_ori):
     time_needed = turn_for_n_degrees(0, 0, -90)
     time.sleep(time_needed)
     # # go for 1 second
-    go_straight_n_seconds(0,0,1)
+    go_straight_n_seconds(0,0,2)
     time.sleep(1)
     # # turn left 90 degrees
     turn_for_n_degrees(0, 0, -90)
