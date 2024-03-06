be948814d4bd71e8b16bbf20045b9538b8672107
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-06 16:46:31 +0100

    update

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -193,8 +193,8 @@ def control_car(dry_run=False):
         angular_v = controller(current_position) - 3.14
         #current setup works
         linear_v = 400
-        angular_v *=20
-        if (current_position < (image.shape[1] / 6)) or (current_position > (image.shape[1] - image.shape[1] / 6)):
+        angular_v *=25
+        if (current_position < (image.shape[1] / 7)) or (current_position > (image.shape[1] - image.shape[1] / 7)):
             linear_v = 0
             angular_v = angular_v * 5
         elif (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
