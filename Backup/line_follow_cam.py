051a693171d7377303a29324171972ef43b5bbea
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-06 17:05:41 +0100

    update

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -109,7 +109,7 @@ def analyze_image(image, prev_value):
     middle = int(base_line.shape[0] / 2)
 
     root_pos, root_index = find_white_pix(base_line, middle)
-    middle_pos, middle_index = find_white_pix(binary_img[-20], middle)
+    middle_pos, middle_index = find_white_pix(binary_img[-15], middle)
 
     current_value = 0
 
@@ -192,7 +192,7 @@ def control_car(dry_run=False):
 
         angular_v = controller(current_position) - 3.14
         #current setup works
-        linear_v = 400
+        linear_v = 400 - np.abs(angular_v) *10
         angular_v *=20
         if (current_position < (image.shape[1] / 7)) or (current_position > (image.shape[1] - image.shape[1] / 7)):
             print("sharp truning")
