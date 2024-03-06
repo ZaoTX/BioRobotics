bad629e34549569f02ee3b2271784169800d28ab
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-06 13:36:19 +0100

    update

--- Ziyao/line_follow_cam.py
+++ Ziyao/line_follow_cam.py
@@ -352,16 +352,16 @@ def control_car(dry_run=False):
 
         image_gray,image_ori = get_image(cap, killer)
         qrcode_detected, time_needed = detect_qrcode(image_gray, linear_v, angular_v)
-        duck_detected = detect_yellow_area(image_ori)
-        if qrcode_detected:
-            time.sleep(time_needed)
-            #print("Camera paused for" + str(time_needed))
-
-        elif duck_detected:
-            # Pause the camera capture
-            avoid_duck(linear_v, angular_v)
-            time.sleep(5)
-            #print("Camera paused for" + str(5))
+        #duck_detected = detect_yellow_area(image_ori)
+        # if qrcode_detected:
+        #     time.sleep(time_needed)
+        #     #print("Camera paused for" + str(time_needed))
+        #
+        # elif duck_detected:
+        #     # Pause the camera capture
+        #     avoid_duck(linear_v, angular_v)
+        #     time.sleep(5)
+        #     #print("Camera paused for" + str(5))
         current_position = analyze_image(image_gray, current_position)
         #print(f"current line position: {current_position}")
         elipsed_time = time.time() - start_time
