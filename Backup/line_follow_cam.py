eeb09ccf0c4a8cf5637667e7b704dc8906551855
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-06 17:09:34 +0100

    update

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -194,17 +194,9 @@ def control_car(dry_run=False):
         #current setup works
         linear_v = 400 - np.abs(angular_v) *10
         angular_v *=25
-        if (current_position < (image.shape[1] / 7)) or (current_position > (image.shape[1] - image.shape[1] / 7)):
-            print("sharp truning")
+        if (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
             linear_v = 0
             angular_v = angular_v * 5
-        elif (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
-            linear_v = 0
-            angular_v = angular_v * 3
-        elif (current_position < (image.shape[1] / 4)) or (current_position > (image.shape[1] - image.shape[1] / 4)):
-
-            linear_v = linear_v -50
-            angular_v = angular_v * 2
         if not dry_run:
             set_car_control(linear_v, angular_v)
             #print(f"Set speed lin: {linear_v}, ang: {angular_v}")
