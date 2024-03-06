288642429136974d15e8b7aea54e96b1688e472e
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-06 16:43:43 +0100

    update

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -109,7 +109,7 @@ def analyze_image(image, prev_value):
     middle = int(base_line.shape[0] / 2)
 
     root_pos, root_index = find_white_pix(base_line, middle)
-    middle_pos, middle_index = find_white_pix(binary_img[-50], middle)
+    middle_pos, middle_index = find_white_pix(binary_img[-20], middle)
 
     current_value = 0
 
