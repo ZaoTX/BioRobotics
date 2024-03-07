29e90ad06ab4275869fdb6b7419d9ac12710476c
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-07 13:13:57 +0100

    update

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -209,10 +209,10 @@ def control_car(dry_run=False):
             angular_v *= 30
             linear_v = int(500 - np.abs(angular_v))
         elif np.abs(angular_v) <3:
-            angular_v *= 40
+            angular_v *= 45
             linear_v = int(450 - np.abs(angular_v))
         else :
-            angular_v *= 40
+            angular_v *= 45
             linear_v = 350
 
 
