21e63ed076ac073954d6b36f8f213d5f0528918a
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-07 18:15:58 +0100

    try to fix the setups for optional tasks

--- Backup/Mindduck.py
+++ Backup/Mindduck.py
@@ -130,7 +130,7 @@ def detect_yellow_area(image):
     # if len(keypoints) > 1:
     #     print("Yellow detected in the image!")
     #     return True
-    if num_white_pixels > 1:
+    if num_white_pixels > 16:
         print("Yellow detected in the image!")
         return True
 def control_car(dry_run=False):
