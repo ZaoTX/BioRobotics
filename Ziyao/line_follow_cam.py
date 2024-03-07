47a646dd2f0103ed0108b0b2505fcbbb69f693cc
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-07 17:50:32 +0100

    try to fix the setups for optional tasks

--- Ziyao/line_follow_cam.py
+++ Ziyao/line_follow_cam.py
@@ -350,7 +350,7 @@ def control_car(dry_run=False):
     while not killer.kill_now:
         if duck_detected:
             stop_car()
-            time.sleep(0.5)
+            time.sleep(1)
             print("car stopped")
         elif qrcode_detected:
             time_needed = qrcode_perform_action(action)
