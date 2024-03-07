e8c843fca20b77b93667e7998abf65837478024c
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-07 13:53:45 +0100

    update

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -223,9 +223,9 @@ def control_car(dry_run=False):
         #     linear_v = 0
         #     angular_v = angular_v * 5
         # el
-        if (current_position < (image.shape[1] / 6)) or (current_position > (image.shape[1] - image.shape[1] / 6)):
+        if (current_position < (image.shape[1] / 7)) or (current_position > (image.shape[1] - image.shape[1] / 7)):
             linear_v = 0
-            angular_v = angular_v * 2.5
+            angular_v = angular_v * 3
         # elif (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
         #     linear_v = 0
         #     angular_v = angular_v * 2
