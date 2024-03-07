5865033bf5c80308a6f8d83c34366171a7795d54
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-07 13:17:04 +0100

    update

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -193,18 +193,19 @@ def control_car(dry_run=False):
 
         angular_v = controller(current_position) - 3.14
         #current setup works
-        if np.abs(angular_v) < 0.24:
-            angular_v *= 0
-            linear_v = int(600)
-        elif np.abs(angular_v) < 0.314:
-            angular_v *= 30
-            linear_v = int(570-(np.abs(angular_v)*0.3))
-        elif np.abs(angular_v) < 1:
+        # if np.abs(angular_v) < 0.24:
+        #     angular_v *= 0
+        #     linear_v = int(600)
+        # el
+        if np.abs(angular_v) < 0.314:
             angular_v *= 20
-            linear_v = int(500 - np.abs(angular_v))
+            linear_v = int(600)
+        elif np.abs(angular_v) < 1:
+            angular_v *= 30
+            linear_v = int(550 - np.abs(angular_v))
         elif np.abs(angular_v) <2:
             angular_v *= 30
-            linear_v = int(500 - np.abs(angular_v))
+            linear_v = int(550 - np.abs(angular_v)*1.1)
         elif np.abs(angular_v) < 2.5:
             angular_v *= 30
             linear_v = int(500 - np.abs(angular_v))
