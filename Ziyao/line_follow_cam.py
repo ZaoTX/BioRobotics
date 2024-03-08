1de9615a86131991827d10314a1f0c700a183d86
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-08 16:53:07 +0100

    update

--- Ziyao/line_follow_cam.py
+++ Ziyao/line_follow_cam.py
@@ -347,7 +347,7 @@ def control_car(dry_run=False):
             #start_time = time.time()
             angular_v = controller(current_position) - 3.14
             #current setup works
-            linear_v = 250
+            linear_v = 270
             angular_v *=20
             if (current_position < (image_gray.shape[1] / 5)) or (current_position > (image_gray.shape[1] - image_gray.shape[1] / 5)):
                 linear_v = 0
