bc4e8f180f6ecff5ee72a95509d85d96422efb28
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-08 16:21:04 +0100

    update

--- Ziyao/line_follow_cam.py
+++ Ziyao/line_follow_cam.py
@@ -353,9 +353,10 @@ def control_car(dry_run=False):
 
         image_gray,image_ori = get_image(cap, killer)
         # qrcode_detected, action = detect_qrcode(image_gray, linear_v, angular_v)
-        duck_detected = detect_yellow_area(image_ori)
 
         current_position = analyze_image(image_gray, current_position)
+
+        duck_detected = detect_yellow_area(image_ori)
 
     set_speed(0, 0)
     print("process terminated")
