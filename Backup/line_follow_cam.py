a696d3988aedbf2a466486cb8804ce4d2a4fe305
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-11 15:10:21 +0100

    comment out qr code detection

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -209,7 +209,7 @@ def control_car(dry_run=False):
         #     linear_v = int(550 - np.abs(angular_v))
         else :
             angular_v *= 40
-            linear_v = 450
+            linear_v = 400
 
 
         # if linear_v <300:
