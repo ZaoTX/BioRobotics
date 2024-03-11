9e1340e334f04ee35a10220047b1d4489903cae0
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-11 16:59:18 +0100

    Add rotate camera

--- Ziyao/line_follow_optional_tasks.py
+++ Ziyao/line_follow_optional_tasks.py
@@ -151,7 +151,7 @@ def get_image(cap, killer):
         return np.zeros((480, 640))
     frame = frame.astype("uint8")
     rotated_img = rotate(frame, angle=-45, reshape=False, mode='nearest')
-    rotated_image_filled = np.where(rotated_image == 0, 255, rotated_img)
+    rotated_image_filled = np.where(rotated_img == 0, 255, rotated_img)
     frame = cv2.cvtColor(rotated_image_filled, cv2.COLOR_BGR2GRAY)
 
     # save last frame
