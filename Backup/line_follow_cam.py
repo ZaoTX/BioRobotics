ff0750e89c46f9bd761f7994700e90c3b5c0ea77
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-11 14:35:03 +0100

    comment out qr code detection

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -222,7 +222,7 @@ def control_car(dry_run=False):
             angular_v = angular_v * 4
         elif (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
             linear_v = 0
-            angular_v = angular_v * 1.5
+            angular_v = angular_v * 2
         if not dry_run:
             set_car_control(linear_v, angular_v)
             #print(f"Set speed lin: {linear_v}, ang: {angular_v}")
