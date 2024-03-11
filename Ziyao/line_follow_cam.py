5e7b7bb1e4e525fc80c8a025c0c0a6c3a84d6e6d
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-11 15:46:37 +0100

    Keep this backup for speed run

--- Ziyao/line_follow_cam.py
+++ Ziyao/line_follow_cam.py
@@ -364,13 +364,11 @@ def control_car(dry_run=False):
         #print(f"Set speed lin: {linear_v}, ang: {angular_v}")
 
         image_gray,image_ori = get_image(cap, killer)
-        qrcode_detected, action = detect_qrcode(image_gray, detector)
+        #qrcode_detected, action = detect_qrcode(image_gray, detector)
 
         current_position = analyze_image(image_gray, current_position)
 
-
-
-        #duck_detected = detect_yellow_area(image_ori,last_duck_detected)
+        duck_detected = detect_yellow_area(image_ori,last_duck_detected)
 
     set_speed(0, 0)
     print("process terminated")
