07ef1b0dbdbbd2affcca3b6e27a71e237edf2087
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-11 15:45:14 +0100

    Keep this backup for speed run

--- Ziyao/line_follow_cam.py
+++ Ziyao/line_follow_cam.py
@@ -355,9 +355,9 @@ def control_car(dry_run=False):
             #current setup works
             linear_v = 300
             angular_v *=30
-            if (current_position < (image_gray.shape[1] / 5)) or (current_position > (image_gray.shape[1] - image_gray.shape[1] / 5)):
-                linear_v = 0
-                angular_v = angular_v * 3
+            # if (current_position < (image_gray.shape[1] / 5)) or (current_position > (image_gray.shape[1] - image_gray.shape[1] / 5)):
+            #     linear_v = 0
+            #     angular_v = angular_v * 3
 
             if not dry_run:
                 set_car_control(linear_v, angular_v)
