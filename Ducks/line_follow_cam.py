182cf679da489d1b5011172f6edb7403b0f175ec
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-11 14:48:29 +0100

    comment out qr code detection

--- Ducks/line_follow_cam.py
+++ Ducks/line_follow_cam.py
@@ -198,9 +198,12 @@ def control_car(dry_run=False):
         #current setup works
         linear_v = 400
         angular_v *=30
-        if (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
+        if (current_position < (image.shape[1] / 7)) or (current_position > (image.shape[1] - image.shape[1] / 7)):
             linear_v = 0
-            angular_v = angular_v * 2
+            angular_v = angular_v * 5
+        elif (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
+            linear_v = 0
+            angular_v = angular_v * 3
 
         if not dry_run:
             set_car_control(linear_v, angular_v)
