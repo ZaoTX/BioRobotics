4b08a5fe024f615d7cd6d880c1554c300393fade
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-12 13:56:56 +0100

    Add rotate camera

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -103,11 +103,11 @@ def find_white_pix(line, middle_idx):
 
 
 def analyze_image(image, prev_value):
-    img_bottom = image[-100:, :]
+    img_bottom = image[-150:, :]
     blur = cv2.GaussianBlur(img_bottom, (5, 5), 0)
     ret, binary_img = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-    base_line = binary_img[-1]
+    base_line = binary_img[-3]
     middle = int(base_line.shape[0] / 2)
 
     root_pos, root_index = find_white_pix(base_line, middle)
