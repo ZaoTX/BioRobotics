e9564fdd67926ec72273a6ea353b9c3c28aebffc
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-12 14:01:56 +0100

    Add rotate camera

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -208,7 +208,7 @@ def control_car(dry_run=False):
 
         if (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
             linear_v = 0
-            angular_v = angular_v * 3
+            angular_v = angular_v * 2
             # if (current_position < (image.shape[1] / 7)) or (current_position > (image.shape[1] - image.shape[1] / 7)):
             #     angular_v = angular_v * 1.5
 
