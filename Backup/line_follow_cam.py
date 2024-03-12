f06960d717ccf583de6f64886f8843c7c54af8e4
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-12 15:52:39 +0100

    Try to run faster

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -193,7 +193,7 @@ def control_car(dry_run=False):
         angular_v = controller(current_position) - 3.14
         #current setup works
         if np.abs(angular_v) <= 0.5:
-            angular_v *= 25
+            angular_v *= 20
             linear_v = int(650)
         else :
             angular_v *= 40
