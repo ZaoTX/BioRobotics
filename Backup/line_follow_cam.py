1ebada771402bdb337b6242a3a5c8231c683e83f
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-12 15:11:01 +0100

    Try to run faster

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -103,7 +103,7 @@ def find_white_pix(line, middle_idx):
 
 
 def analyze_image(image, prev_value):
-    img_bottom = image[-100:, :]
+    img_bottom = image[-150:, :]
     blur = cv2.GaussianBlur(img_bottom, (5, 5), 0)
     ret, binary_img = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
