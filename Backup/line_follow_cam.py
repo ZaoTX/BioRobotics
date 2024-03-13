671aa66b5bea4bb4b9fe54efbf7483a54f7ea115
he.ziyao <he.ziyao@uni-konstanz.de>
2024-03-13 15:21:00 +0100

    Try to run faster

--- Backup/line_follow_cam.py
+++ Backup/line_follow_cam.py
@@ -203,7 +203,7 @@ def control_car(dry_run=False):
         #     linear_v = 350
 
         angular_v *= 30
-        linear_v = 350
+        linear_v = 300
         if (current_position < (image.shape[1] / 5)) or (current_position > (image.shape[1] - image.shape[1] / 5)):
             linear_v = 0
             angular_v = angular_v * 3
